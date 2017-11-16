df18c2fceff27a1657ffe9eceaca74b28bec52ca
Roman Bardachev <sinon_u@ukr.net>
2017-11-17 00:01:45 +0200

    Page objects and tests

---
+++ AutomationFramework/Tests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using NUnit.Framework;
+using AutomationFramework.PageObjects;
+
+namespace AutomationFramework.Tests
+{
+    class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void SearchForJob()
+        {
+            FindJobsPage findJobs = new FindJobsPage(driver);
+            findJobs.GoToFindJobsPage();
+            findJobs.SearchForJobs("QA");
+        }
+
+        [Test]
+        public void SearchForACompanyReview()
+        {
+            CompanyReviewsPage companyReviewPage = new CompanyReviewsPage(driver);
+            companyReviewPage.GoToCompanyReviewPage();
+            companyReviewPage.SearchForACompanyReview("Manitoba Hydro");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
